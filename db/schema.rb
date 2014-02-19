a0fcbf6d2d398f2ffdc80399708b60429b9fae3b
Nima Gardideh <nima@halfmoon.ws>
2014-02-19 18:18:10 -0500

    Added postgres + remove sqlite.

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20140219023813) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "events", force: true do |t|
     t.datetime "created"
     t.datetime "start_time"
